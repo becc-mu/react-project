eb25ec279f8e4872b2f3e01427f28ad6e93c72f7
becc-mu <becc.mulugeta@gmail.com>
2019-12-10 13:05:30 +0000

    add new component and modified index.js

---
+++ components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test() {
+  return(
+    <h1>Hello Testing if my newly created component works</h1>
+  )
+}
+
+export default Test
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import Test from './components/Test'
 
 ReactDom.render(
-  <h1>Hello World with JSX, Babel and Webapck</h1>,
+  // <h1>Hello World with JSX, Babel and Webapck</h1>,
+  <Test />,
   document.getElementById("root")
 );
