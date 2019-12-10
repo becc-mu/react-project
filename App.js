afe34bafa0a3e62e3f527e73449b3f4bc9a2c202
becc-mu <becc.mulugeta@gmail.com>
2019-12-10 17:05:31 +0000

    added dynamic question rendering

--- App.js
+++ App.js
@@ -5,19 +5,22 @@ function App() {
   return(
     <div >
       <Joke
-        q={{question: "how did the chicken cross the road?", punchline: "quack quack"}}
+        question="how did the chicken cross the road?"
+        punchline="quack quack"
       />
       <Joke
-        q={{question: "", punchline: "I used to think the brain was the most important organ. Then I thought, look what’s telling me that."}}
+        punchline="I used to think the brain was the most important organ. Then I thought, look what’s telling me that."
       />
       <Joke
-        q={{question: "Why does the leopard find it difficult to hide and stalk?", punchline: "Because he is spotted always."}}
+        question="Why does the leopard find it difficult to hide and stalk?"
+        punchline="Because he is spotted always."
+        />
+      <Joke
+        punchline="The midget fortune teller who kills his customers is a small medium at large."
       />
       <Joke
-      q={{question: "", punchline: "The midget fortune teller who kills his customers is a small medium at large."}}
-      />
-      <Joke
-        q={{question: "which weighs more, a ton of feathers or a ton of bricks?", punchline: "They both weigh the same"}}
+        question="which weighs more, a ton of feathers or a ton of bricks?"
+        punchline="They both weigh the same"
       />
     </div>
   );
--- components/Joke.js
+++ components/Joke.js
@@ -3,8 +3,9 @@ import React from 'react'
 function Joke(props) {
   return(
     <div>
-      <h2>{props.q.question}</h2>
-      <h2>{props.q.punchline}</h2>
+      <h2 style={{display: props.question ? "block" : "none"}}>Question: {props.question}</h2>
+      <h2>Answer: {props.punchline}</h2>
+      <hr/>
     </div>
   )
 }
