7aca27021bd102e1e44bfb710f8cb60e8f268cc5
becc-mu <becc.mulugeta@gmail.com>
2019-12-10 17:18:16 +0000

    some inline styling

--- components/Joke.js
+++ components/Joke.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { gray } from 'ansi-colors'
 
 function Joke(props) {
   return(
     <div>
       <h2 style={{display: props.question ? "block" : "none"}}>Question: {props.question}</h2>
-      <h2>Answer: {props.punchline}</h2>
+      <h2 style={{color: !props.answer && "#888888"}}>Answer: {props.punchline}</h2>
       <hr/>
     </div>
   )
